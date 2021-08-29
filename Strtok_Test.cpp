7dbcdd2ca6aab638d1cade0543d0712ed65aeea0
PranavKumar27 <pranav.jenus@gmail.com>
2021-08-29 23:41:17 +0530

    Update Strtok_Test.cpp

--- Strtok_Test.cpp
+++ Strtok_Test.cpp
@@ -1,1 +1,21 @@
+#include<iostream>
+#include<cstring>
 
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+	char str[]="This is a Bright and Sunny Day!!";
+	
+	char *token=strtok(str," ");
+	
+	while(token!=NULL)
+	{
+		cout << token << endl;
+		token=strtok(NULL," ");	
+		//cout << token << endl;
+		// Can't print at end because if Null it gives Segmentation fault
+		
+	}	
+}
